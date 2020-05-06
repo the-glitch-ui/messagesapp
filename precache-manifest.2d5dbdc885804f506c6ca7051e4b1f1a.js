169a5e384e068b161ef6e5fee471f4edbce0ccee
Work <work@SRs-iMac.local>
2020-05-06 11:03:08 +0200

    Updates

---
+++ precache-manifest.2d5dbdc885804f506c6ca7051e4b1f1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf3f8bc01ecad69eee37bf48791f643d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1f48ca17ea109f2644e",
+    "url": "/static/css/main.6139505b.chunk.css"
+  },
+  {
+    "revision": "c4abd39d02e9dbf2e5b7",
+    "url": "/static/js/2.9659eaab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9659eaab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1f48ca17ea109f2644e",
+    "url": "/static/js/main.45d9da07.chunk.js"
+  },
+  {
+    "revision": "7379be06f2a9daf35fe2",
+    "url": "/static/js/runtime-main.73a15297.js"
+  }
+]);
